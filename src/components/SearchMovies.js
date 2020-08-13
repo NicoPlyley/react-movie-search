2f576ae26091ff8fd398dc349c2452eef10690e7
Nico Plyley <nplyley@gmail.com>
2020-08-13 01:05:25 -0700

    Added mapping for movie results

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -26,18 +26,38 @@ const SearchMovies = () => {
     }
 
     return (
-        <form className="form" onSubmit={handleSearch}>
-            <label className="label" htmlFor="query">Movie Name</label>
-            <input
-                className="input"
-                type="text"
-                name="query"
-                value={query}
-                onChange={handleChange}
-                placeholder="Search for a movie"
-            />
-            <button className="button" type="submit">Search</button>
-        </form>
+        <div>
+            <form className="form" onSubmit={handleSearch}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input
+                    className="input"
+                    type="text"
+                    name="query"
+                    value={query}
+                    onChange={handleChange}
+                    placeholder="Search for a movie"
+                />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                { movies.filter(movie => movie.poster_path).map(movie => (
+                    <div className="card" key={movie.id}>
+                        <img
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                            alt=""
+                        />
+
+                        <div className="content">
+                            <h3 className="title">{movie.title}</h3>
+                            <p><small>Release Date: {movie.release_date}</small></p>
+                            <p><small>Rating: {movie.vote_average}</small></p>
+                            <p className="desc">{movie.overview}</p>
+                        </div>
+
+                    </div>
+                )) }
+            </div>
+        </div>
     )
 }
 
