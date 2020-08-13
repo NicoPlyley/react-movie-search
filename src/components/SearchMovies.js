3eba032c97855ea9169455029e054943eb9aa89d
Nico Plyley <nplyley@gmail.com>
2020-08-13 00:43:50 -0700

    Created state for query

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './sass/search_movies.scss'
 
 const SearchMovies = () => {
 
+    const [query, setQuery] = useState('')
+
+    const handleChange = (e) => {
+        setQuery(e.target.value)
+        console.log(query)
+    }
+
     const handleSearch = async (e) => {
         e.preventDefault()
 
-        const query = 'Jurassic Park'
         const url = `https://api.themoviedb.org/3/search/movie?api_key=b62146552c38f87f9b092bf28296f87f&language=en-US&query=${query}&page=1&include_adult=false`;
 
         try {
@@ -21,7 +27,14 @@ const SearchMovies = () => {
     return (
         <form className="form" onSubmit={handleSearch}>
             <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query" placeholder="Search for a movie" />
+            <input
+                className="input"
+                type="text"
+                name="query"
+                value={query}
+                onChange={handleChange}
+                placeholder="Search for a movie"
+            />
             <button className="button" type="submit">Search</button>
         </form>
     )
